d8db0a22de4794561983ea259d473d98f6b1e56a
Minhaz Ahmed Rafi <40710320+minhazahmedrafi@users.noreply.github.com>
2021-03-13 01:27:39 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// <!-- Initialize Swiper -->
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
